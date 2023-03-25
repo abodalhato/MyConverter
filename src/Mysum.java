5ae0e26b8f940d4899176366fa49a69f04ece07e
alhato <aalhato@smail.ucas.edu.ps>
2023-03-25 11:00:11 +0300

    new update

---
+++ src/Mysum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Mysum {
+    public static void main(String[] args){
+        Scanner mySc =new Scanner(System.in);
+System.out.println("Enter first ");
+    }
+    static int sum(int num1, int num2){
+        return num1 + num2;
+
+    }
+}
